4a4b03985b6587f3378cd8a52efcad1a7e052db3
Andrey Allyson <andreyuser47@gmail.com>
2023-10-25 17:42:48 -0300

    chore: changed build scripts

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,7 @@
 import { program, Command } from "commander";
 import { CryptorProgramInitializer } from "./initializers/cryptor";
 import { DevBuildProgramInitializer } from "./initializers/dev-build";
-import { ProdBundleProgramInitializer } from "./initializers/prod-bundle";
+import { ProdBuildProgramInitializer } from "./initializers/prod-bundle";
 
 export interface ProgramInitializer {
   initialize(program: Command): void;
@@ -11,7 +11,7 @@ const initializers: ProgramInitializer[] = [];
 
 initializers.push(new CryptorProgramInitializer());
 initializers.push(new DevBuildProgramInitializer());
-initializers.push(new ProdBundleProgramInitializer());
+initializers.push(new ProdBuildProgramInitializer());
 
 for (const initializer of initializers) {
   initializer.initialize(program);
--- scripts/initializers/prod-bundle.ts
+++ scripts/initializers/prod-bundle.ts
@@ -1,22 +1,46 @@
 import { Command } from 'commander';
 import { build } from 'esbuild';
 import { ProgramInitializer } from 'index';
+import cp from 'child_process';
 
-export class ProdBundleProgramInitializer implements ProgramInitializer {
+type ExecResult = {
+  stdout: string;
+  stderr: string;
+};
+
+function exec(command: string) {
+  return new Promise<ExecResult>((resolve, reject) => {
+    cp.exec(command, (err, stdout, stderr) => {
+      if (err !== null) reject(err);
+
+      resolve({
+        stderr,
+        stdout,
+      });
+    });
+  });
+}
+
+export class ProdBuildProgramInitializer implements ProgramInitializer {
   initialize(program: Command): void {
     program
       .command('build:prod')
       .description('build for production')
       .action(async () => {
-        await build({
-          entryPoints: [
-            './src/renderer/index.ts'
-          ],
-          outdir: './public',
-          target: 'es2020',
-          bundle: true,
-          minify: true,
-        });
+        await Promise.all([
+          build({
+            entryPoints: [
+              './src/renderer/index.ts'
+            ],
+            outdir: './public',
+            target: 'es2020',
+            bundle: true,
+            minify: true,
+          }),
+          exec('npx tsc -p src/app'),
+        ]);
+
+        console.log('Builded with success');
       });
   }
 }
