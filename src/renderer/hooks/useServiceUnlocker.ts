9bce30a21bc60b489a0620165959b9f5350d3a15
Andrey Allyson <andreyuser47@gmail.com>
2023-10-27 23:03:21 -0300

    fix: fixed 'useServiceUnlocker' isLocked state

---
+++ src/renderer/hooks/useServiceUnlocker.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { AppError, api } from "../api";
+
+export function useServiceUnlocker() {
+  const [isLocked, setLocked] = useState(true);
+
+  async function requestIsLocked(): Promise<boolean> {
+    const isLocked = await api.isServicesLocked();
+    if (!isLocked.ok) AppError.throwError(isLocked.error);
+
+    if (isLocked.data === true) {
+      alert('Services still locked!');
+    }
+
+    return isLocked.data;
+  }
+
+  useEffect(() => {
+    requestIsLocked()
+      .then(isLocked => {
+        if (isLocked) return;
+
+        setLocked(false);
+      });
+  }, []);
+
+  async function unlock(password: string) {
+    const result = await api.unlockServices(password);
+    if (result.ok === false) {
+      AppError.log(result.error);
+      return;
+    }
+
+    const isLocked = await requestIsLocked();
+    if (isLocked) return;
+
+    setLocked(false);
+  }
+
+  return { isLocked, unlock };
+}
