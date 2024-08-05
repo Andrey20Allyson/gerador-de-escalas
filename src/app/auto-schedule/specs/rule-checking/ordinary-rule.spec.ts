dbf1c4d2b40da708c9ef92021f43042234383b75
Andrey20Allyson <andreyuser47@gmail.com>
2024-08-05 18:56:25 -0300

    test: adiciona teste para o ajuste na regra da ordinária

--- src/app/auto-schedule/specs/rule-checking/ordinary-rule.spec.ts
+++ src/app/auto-schedule/specs/rule-checking/ordinary-rule.spec.ts
@@ -3,6 +3,14 @@ import { OrdinaryAssignmentRule } from "../../extra-duty-lib/builders/rule-check
 import { mock } from "../mocking/mocker";
 import { iterRange } from "../../utils";
 import { WorkTime } from "../../extra-duty-lib/structs/work-time";
+import { DaysOfWork } from "../../extra-duty-lib/structs/days-of-work";
+
+interface DutyPositionSizeTestConfig {
+  day: number;
+  duty: number;
+  dutyPositionSize: number;
+  result: boolean;
+}
 
 describe(OrdinaryAssignmentRule.name, () => {
   const checker = new OrdinaryAssignmentRule();
@@ -30,6 +38,60 @@ describe(OrdinaryAssignmentRule.name, () => {
 
     expect(checker.canAssign(worker, duty))
       .toBeFalsy();
+  });
+
+  test(`Should use dutyPositionSize to calculate the time off`, () => {
+    const month = mock.month();
+
+    const tests: DutyPositionSizeTestConfig[] = [{
+      day: 6,
+      duty: 2,
+      dutyPositionSize: 2,
+      result: false,
+    }, {
+      day: 6,
+      duty: 3,
+      dutyPositionSize: 2,
+      result: true,
+    }, {
+      day: 6,
+      duty: 2,
+      dutyPositionSize: 1,
+      result: true,
+    }, {
+      day: 6,
+      duty: 3,
+      dutyPositionSize: 3,
+      result: false,
+    }, {
+      day: 4,
+      duty: 2,
+      dutyPositionSize: 2,
+      result: true,
+    }, {
+      day: 4,
+      duty: 3,
+      dutyPositionSize: 2,
+      result: false,
+    }];
+
+    const { table, worker } = mock({
+      worker: {
+        workTime: new WorkTime(7, 24),
+        daysOfWork: DaysOfWork.fromDays([5], month.year, month.index),
+      },
+      month,
+    });
+
+    for (const config of tests) {
+      const day = table.getDay(config.day);
+      const duty = day.getDuty(config.duty);
+
+      table.config.dutyPositionSize = config.dutyPositionSize;
+
+      expect(checker.canAssign(worker, duty)).toStrictEqual(config.result);
+    }
+    
   });
 
   test(`Shold return true if duty don't collides with worker's ordinary`, () => {
